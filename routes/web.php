2f0d9b32bfa671fa1cc7f306481a6b9f378fdf54
Al Imran Ahmed <al.imran.cse@gmail.com>
2026-02-25 23:18:40 +0100

    Component maded to pages

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::livewire('games', 'game.index')->name('game.index');
-    Route::livewire('games/{slug}', 'game.show')->name('game.show');
+    Route::livewire('games', 'pages::game.index')->name('game.index');
+    Route::livewire('games/{slug}', 'pages::game.show')->name('game.show');
 });
 
 require __DIR__.'/settings.php';
